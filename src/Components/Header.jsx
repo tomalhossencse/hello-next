eed1bfcb071b50aa91a2f3541834ef527b66bfbb
Tomal Hossen <tomalhossen78@gmail.com>
2026-01-04 08:56:55 +0600

    handle conditional layouts done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,11 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
-
 const Header = () => {
+  const pathname = usePathname();
+  console.log(pathname);
+  if (pathname.startsWith("/dashboard")) return <></>;
   return (
     <header className="px-3 py-4 border-b-2 border-gray-200 flex flex-wrap justify-between items-center">
       <Link href={"/"}>📘Dev-Story</Link>
